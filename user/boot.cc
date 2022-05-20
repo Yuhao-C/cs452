fce28faec9e61444e29e8a3585dcdc226478a947
Qishen Wu <qishen.wu@uwaterloo.ca>
2022-05-19 23:42:01 -0400

    finish boot task

--- user/boot.cc
+++ user/boot.cc
@@ -5,10 +5,10 @@
 #include "user/task.h"
 
 void boot() {
-  // for (int i = 0; i < 8; i += 2) {
-  //   int tid = create(i, k1);
-  //   bwprintf(COM2, "Created: %d\n\r", tid);
-  // }
+  for (int i = 0; i < 8; i += 2) {
+    int tid = create(i, k1);
+    bwprintf(COM2, "Created: %d\n\r", tid);
+  }
   bwputstr(COM2, "FirstUserTask: exiting\n\r");
   exit();
 }
