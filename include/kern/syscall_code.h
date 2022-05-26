8c3d81be4787641b9611f4569bc35ce0da043773
Yuhao Chen <y627chen@uwaterloo.ca>
2022-05-26 16:14:51 -0400

    SYSCALL_FUNC macro

--- include/kern/syscall_code.h
+++ include/kern/syscall_code.h
@@ -6,5 +6,17 @@
 #define SYS_PARENT_TID 2
 #define SYS_YIELD 3
 #define SYS_EXIT 4
+#define SYS_SEND 5
+#define SYS_RECEIVE 6
+#define SYS_REPLY 7
+
+#define SYSCALL_FUNC(name, code) \
+  .text;                         \
+  .align 2;                      \
+  .global name;                  \
+  .type name, % function;        \
+  name:;                         \
+  swi code;                      \
+  bx lr;
 
 #endif  // KERN_SYSCALL_CODE_H_
