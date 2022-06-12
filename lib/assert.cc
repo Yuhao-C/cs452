04e0e67c591ca1235216447e9429ca71553319a9
Yuhao Chen <y627chen@uwaterloo.ca>
2022-06-12 15:00:12 -0400

    use io lib for user assert

--- lib/assert.cc
+++ lib/assert.cc
@@ -2,6 +2,7 @@
 
 #include "kern/sys.h"
 #include "lib/bwio.h"
+#include "lib/io.h"
 #include "user/task.h"
 
 void __k_assert_func(const char* file, int line, const char* func,
@@ -18,12 +19,12 @@ void __k_assert_func(const char* file, int line, const char* func,
 
 void __assert_func(const char* file, int line, const char* func,
                    const char* cond) {
-  bwprintf(COM2,
-           "\033[31m"
-           "Assertion failed at %s:%d\n\r"
-           "Function: %s\n\r"
-           "Condition: %s\n\r"
-           "\033[0m",
-           file, line, func, cond);
+  printf(COM2,
+         "\033[31m"
+         "Assertion failed at %s:%d\n\r"
+         "Function: %s\n\r"
+         "Condition: %s\n\r"
+         "\033[0m",
+         file, line, func, cond);
   exit();
 }
