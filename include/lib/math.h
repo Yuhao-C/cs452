36af11e722cd3188c146a9f736526847d6945aef
Yuhao Chen <y627chen@uwaterloo.ca>
2022-06-13 14:58:39 -0400

    Add mod function

--- include/lib/math.h
+++ include/lib/math.h
@@ -1,1 +1,8 @@
+#ifndef LIB_MATH_H_
+#define LIB_MATH_H_
+
 unsigned int log2(unsigned int x);
+
+int mod(int a, int b);
+
+#endif  // LIB_MATH_H_
--- lib/math.cc
+++ lib/math.cc
@@ -3,3 +3,11 @@
 unsigned int log2(unsigned int x) {
   return sizeof(unsigned int) * 8 - __builtin_clz(x) - 1;
 }
+
+int mod(int a, int b) {
+  int m = a % b;
+  if (m < 0) {
+    m = (b < 0) ? m - b : m + b;
+  }
+  return m;
+}
