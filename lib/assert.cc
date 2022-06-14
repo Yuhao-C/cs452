7a75ac16eb2231c96eb1f87676493b418e74a50a
Yuhao Chen <y627chen@uwaterloo.ca>
2022-06-14 16:24:37 -0400

    update user assert

--- lib/assert.cc
+++ lib/assert.cc
@@ -3,6 +3,7 @@
 #include "kern/sys.h"
 #include "lib/bwio.h"
 #include "lib/io.h"
+#include "user/sys.h"
 #include "user/task.h"
 
 void __k_assert_func(const char* file, int line, const char* func,
@@ -19,12 +20,12 @@ void __k_assert_func(const char* file, int line, const char* func,
 
 void __assert_func(const char* file, int line, const char* func,
                    const char* cond) {
-  printf(COM2,
-         "\033[31m"
-         "Assertion failed at %s:%d\n\r"
-         "Function: %s\n\r"
-         "Condition: %s\n\r"
-         "\033[0m",
-         file, line, func, cond);
-  exit();
+  bwprintf(COM2,
+           "\033[31m"
+           "Assertion failed at %s:%d\n\r"
+           "Function: %s\n\r"
+           "Condition: %s\n\r"
+           "\033[0m",
+           file, line, func, cond);
+  shutdown();
 }
